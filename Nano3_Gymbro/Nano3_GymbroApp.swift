3229f664aaf91cc1d93639d0e149227e775f9bdd
Ragnarom-kun <romi.fadhurrohman@gmail.com>
2024-07-08 11:45:58 +0700

    feat: init swift project

---
+++ Nano3_Gymbro/Nano3_GymbroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano3_GymbroApp.swift
+//  Nano3_Gymbro
+//
+//  Created by Romi Fadhurohman Nabil on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano3_GymbroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
