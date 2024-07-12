49266e0797389050ad562e37b92caf342df28b77
Christian Gunawan <cg404cgunawan@gmail.com>
2024-07-12 10:26:11 +0700

    iterasi 3 tinggal backend ama chart

--- Nano3_Gymbro/View/PRCard/PRDetailView.swift
+++ Nano3_Gymbro/View/PRCard/PRDetailView.swift
@@ -12,23 +12,35 @@ struct DetailView: View {
     let cards: [Card]
     @State private var selectedItem = 0
     @State private var selection = 0
-    @State private var options: [String] = []
+    @State private var options: [String] = ["Option 1", "Option 2", "Option 3"] // Example options
     let segments = ["D", "W", "M", "Y"]
-    
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack {
-                    VStack(alignment:.center,spacing: 16){
+            
+            Section {
+                List {
+                    Section {
                         Spacer()
-                        Image(systemName: "dumbbell.fill").foregroundColor(.orange).font(.system(size: 80))
-                        HStack(alignment:.firstTextBaseline) {
+                        HStack {
+                            Spacer()
+                            Image(systemName: "dumbbell.fill").foregroundColor(.orange).font(.system(size: 80))
+                            Spacer()
+                        }
+                        .listRowSeparator(.hidden)
+                        
+                        HStack(alignment: .firstTextBaseline) {
                             Spacer()
                             Text("\(card.weight)").font(.system(size: 34)).bold()
                             Text("Kg").font(.system(size: 15))
                             Spacer()
                         }
-                        Text("Last Updated \(card.date, formatter: dateFormatter)").font(.system(size: 15))
+                        .listRowSeparator(.hidden)
+                        
+                        HStack {
+                            Spacer()
+                            Text("Last Updated \(card.date, formatter: dateFormatter)").font(.system(size: 15)).fontWeight(.thin)
+                            Spacer()
+                        }
+                        .listRowSeparator(.hidden)
                         
                         Button(action: {
                             print("Button tapped!")
@@ -46,75 +58,180 @@ struct DetailView: View {
                             .cornerRadius(10)
                             .padding()
                         }
-                        Spacer()
+                        .listRowSeparator(.hidden)
                     }
-                    .background(Color("ContainerColor"))
-                    .padding(10)
-                    .cornerRadius(50)
-                }
-                
-                VStack(spacing:16){
-                    HStack {
-                        Spacer()
+                    
+                    Section {
                         HStack(alignment: .firstTextBaseline) {
                             Image(systemName: "flame")
                             Text("Your Progress").bold()
+                            Spacer()
+                            Picker(selection: $selectedItem, label: Text("")) {
+                                ForEach(0 ..< options.count, id: \.self) {
+                                    Text(self.options[$0])
+                                }
+                            }
+                            .pickerStyle(MenuPickerStyle())
+                            .frame(maxWidth: 150)
                         }
                         .foregroundColor(.orange)
-                        .alignmentGuide(HorizontalAlignment.trailing) { _ in
-                            return UIScreen.main.bounds.width / 2 // Adjust as needed
-                        }
-
-                        Picker(selection: $selectedItem, label: Text("Options")) {
-                            ForEach(0 ..< options.count, id: \.self) {
-                                Text(self.options[$0])
+                        
+                        Picker(selection: $selection, label: Text("Segments")) {
+                            ForEach(0 ..< segments.count) { index in
+                                Text(self.segments[index])
+                                    .tag(index)
                             }
                         }
-                        .pickerStyle(MenuPickerStyle())
-                        .padding()
-                    }
-                    Picker(selection: $selection, label: Text("Segments")) {
-                        ForEach(0 ..< segments.count) { index in
-                            Text(self.segments[index])
-                                .tag(index)
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                    .padding()
-                    
-                    VStack {
-                        HStack{
-                            VStack{
+                        .pickerStyle(SegmentedPickerStyle())
+                        .listRowSeparator(.hidden)
+                        
+                        HStack {
+                            Spacer()
+                            VStack {
                                 Text("Start").foregroundColor(.blue)
-                                Text("\(card.weight) Kg")
+                                Text("\(card.weight).0 Kg")
                             }
                             Divider()
                                 .background(Color.gray)
                                 .frame(height: 30)
-                            VStack{
+                            VStack {
                                 Text("Last").foregroundColor(.blue)
-                                Text("\(card.weight) Kg")
+                                Text("\(card.weight).0 Kg")
+                                
+                               
                             }
+                            Spacer()
                         }
                         
-                           Text("Ini Chart")
-
+                        Text("chart").font(.system(size: 800))
                     }
-                    Spacer()
-                }.background(Color("ContainerColor"))
-                    .padding(10)
-                    .cornerRadius(50)
+                    
+                    
+                }
+                 
+            }.navigationTitle("Personal Record")
                 
+            .onAppear {
+                            self.options = cards.map { $0.title }
+                        }
             }
-            .padding(.bottom,1000)
-            .background(Color("BackgroundBG"))
-            .navigationTitle("Personal Record")
-        }
-        .onAppear {
-            self.options = cards.map { $0.title }
-        }
+
+      
     }
-}
+        
+        
+        
+        
+        
+        
+//
+//        NavigationView{
+//            ScrollView {
+//                Section {
+//                    HStack {
+//                        VStack(alignment:.center,spacing: 16){
+//                            Spacer()
+//                            Image(systemName: "dumbbell.fill").foregroundColor(.orange).font(.system(size: 80))
+//                            HStack(alignment:.firstTextBaseline) {
+//                                Spacer()
+//                                Text("\(card.weight)").font(.system(size: 34)).bold()
+//                                Text("Kg").font(.system(size: 15))
+//                                Spacer()
+//                            }
+//                            Text("Last Updated \(card.date, formatter: dateFormatter)").font(.system(size: 15))
+//                            
+//                            Button(action: {
+//                                print("Button tapped!")
+//                            }) {
+//                                HStack {
+//                                    Spacer()
+//                                    Image(systemName: "square.and.pencil")
+//                                    Text("Add New Record")
+//                                    Spacer()
+//                                }
+//                                .padding()
+//                                .background(Color("ButtonContainerBG"))
+//                                .foregroundColor(.blue)
+//                                .font(.headline)
+//                                .cornerRadius(10)
+//                                .padding()
+//                            }
+//                            Spacer()
+//                        }
+//                        .background(Color("ContainerColor"))
+//                        .padding(10)
+//                        .cornerRadius(50)
+//                    }
+//                    
+//                    NavigationView{
+//                        List{
+//                            HStack(alignment: .firstTextBaseline) {
+//                                Image(systemName: "flame")
+//                                Text("Your Progress").bold()
+//                            }
+//                            .foregroundColor(.orange)
+//                        }
+//                    }
+//                    Section{
+//                        HStack {
+//                            Spacer()
+//                            HStack(alignment: .firstTextBaseline) {
+//                                Image(systemName: "flame")
+//                                Text("Your Progress").bold()
+//                            }
+//                            .foregroundColor(.orange)
+//                            
+//                            Picker(selection: $selectedItem, label: Text("Options")) {
+//                                ForEach(0 ..< options.count, id: \.self) {
+//                                    Text(self.options[$0])
+//                                }
+//                            }
+//                            .pickerStyle(MenuPickerStyle())
+//                            .padding()
+//                        }
+//                        Picker(selection: $selection, label: Text("Segments")) {
+//                            ForEach(0 ..< segments.count) { index in
+//                                Text(self.segments[index])
+//                                    .tag(index)
+//                            }
+//                        }
+//                        .pickerStyle(SegmentedPickerStyle())
+//                        .padding()
+//                        
+//                        VStack {
+//                            HStack{
+//                                VStack{
+//                                    Text("Start").foregroundColor(.blue)
+//                                    Text("\(card.weight) Kg")
+//                                }
+//                                Divider()
+//                                    .background(Color.gray)
+//                                    .frame(height: 30)
+//                                VStack{
+//                                    Text("Last").foregroundColor(.blue)
+//                                    Text("\(card.weight) Kg")
+//                                }
+//                            }
+//                            
+//                            Text("Ini Chart")
+//                            
+//                        }
+//                        Spacer()
+//                    }.background(Color("ContainerColor"))
+//                        .padding(10)
+//                        .cornerRadius(50)
+//                    
+//                }
+//                .padding(.bottom,1000)
+//                .background(Color("BackgroundBG"))
+//                .navigationTitle("Personal Record")
+//            }
+//            .onAppear {
+//                self.options = cards.map { $0.title }
+//            }
+//        }
+//    }
+//}
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
