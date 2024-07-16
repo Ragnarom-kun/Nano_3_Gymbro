18788cea964c5af1fce3ff9f8f958ef5e52f29ee
Ragnarom-kun <romi.fadhurrohman@gmail.com>
2024-07-16 12:47:57 +0700

    feat: add router view

--- Nano3_Gymbro/View/Router/RouterView.swift
+++ Nano3_Gymbro/View/Router/RouterView.swift
@@ -4,11 +4,10 @@
 //
 //  Created by Romi Fadhurohman Nabil on 11/07/24.
 //
-
 import SwiftUI
 
 struct RouterView<Content: View>: View {
-    @StateObject var router = Router.shared
+    @StateObject var router: Router = Router()
     // Our root view content
     private let content: Content
     
@@ -19,15 +18,10 @@ struct RouterView<Content: View>: View {
     var body: some View {
         NavigationStack(path: $router.path) {
             content
-                .navigationDestination(for: Router.Destination.self) { route in
-                    routerView(for: route)
+                .navigationDestination(for: Router.Route.self) { route in
+                    router.view(for: route)
                 }
         }
         .environmentObject(router)
-    }
-    
-    @ViewBuilder
-    private func routerView(for destination: Router.Destination) -> some View {
-        RouterViewModel().view(for: destination)
     }
 }
